4c982c3a7fcf9094e3897b25ca1cc4ed898143b8
MTCreator <mike1999v@gmail.com>
2019-02-05 18:36:19 +0000

    worked on site

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function openSidebar() {
+  document.getElementById("sidebar").style.display = "block";
+}
+function closeSidebar() {
+  document.getElementById("sidebar").style.display = "none";
+}
+function applyBetterFont() {
+  document.getElementById("body").className += " betterfont";
+}
